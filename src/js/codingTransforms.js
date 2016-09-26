e4a15a3186a28cf48a1dd181df53eaff4e971e08
Tony Atkins <tony@raisingthefloor.org>
2016-09-26 12:25:56 +0200

    GPII-2053:  Added transformation functions to handle Date <-> RFC 3339 strings.

--- src/js/codingTransforms.js
+++ src/js/codingTransforms.js
@@ -90,3 +90,65 @@ gpii.binder.transforms.objectToString = function (value, transformSpec) {
 fluid.defaults("gpii.binder.transforms.objectToString", {
     gradeNames: ["fluid.standardTransformFunction"]
 });
+
+/**
+ *
+ * Transform a string to a date using the Date constructor.  Accepts (among other things) the date and dateTime values
+ * returned by HTML5 date and dateTime inputs.
+ *
+ * A string that cannot be parsed will be treated as `undefined`.
+ *
+ * @param value - The String value to be transformed into a Date object.
+ * @returns {*}
+ */
+gpii.binder.transforms.stringToDate = function (value) {
+    var date = new Date(value);
+    return isNaN(date.getTime()) ? undefined : date;
+};
+
+fluid.defaults("gpii.binder.transforms.stringToDate", {
+    gradeNames: ["fluid.standardTransformFunction"]
+});
+
+/**
+ *
+ * Transform a Date object into a date string using its toISOString method.  Results in date strings that are suitable
+ * for use with both HTML5 "date" inputs and JSON Schema "date" format string validation.
+ *
+ * A non-date object will be treated as `undefined`.
+ *
+ * @param value - The Date object to be transformed into an ISO 8601 string.
+ * @returns {*}
+ */
+gpii.binder.transforms.dateToString = function (value) {
+    if (value instanceof Date) {
+        var isoString = value.toISOString(); // A string like "2016-09-26T08:05:57.462Z"
+        var dateString = isoString.substring(0, isoString.indexOf("T")); // A string like "2016-09-26"
+        return dateString;
+    }
+    else {
+        return undefined;
+    }
+};
+
+fluid.defaults("gpii.binder.transforms.dateToString", {
+    gradeNames: ["fluid.standardTransformFunction"]
+});
+
+/**
+ *
+ * Transform a Date object into a date/time string using its toISOString method.  Results in date strings that are
+ * suitable for use with both HTML5 "dateTime" inputs and JSON Schema "date-time" format string validation.
+ *
+ * A non-date object will be treated as `undefined`.
+ *
+ * @param value - The Date object to be transformed into an ISO 8601 string.
+ * @returns {*}
+ */
+gpii.binder.transforms.dateTimeToString = function (value) {
+    return value instanceof Date ? value.toISOString() : undefined;
+};
+
+fluid.defaults("gpii.binder.transforms.dateTimeToString", {
+    gradeNames: ["fluid.standardTransformFunction"]
+});
