a776add5f2bd48038f1abcc73581cd1341cff50e
Tony Atkins <tony@raisingthefloor.org>
2016-11-23 13:31:37 +0100

    GPII-2077:  Added instructions for debugging the tests outside of testem.

--- README.md
+++ README.md
@@ -141,5 +141,6 @@ whenever an `onDomChange` event is fired.
 To run the tests in this package, run `npm test`.  In addition to the normal test output, an
 [Istanbul](https://github.com/gotwarlost/istanbul) test coverage report will be saved to `coverage/lcov-report/index.html`.
 
-If you want to view the tests in a browser, you will need to run `npm run instrument` manually first.  You can then
-either run `testem` and open the URL it returns in a browser, or host the content yourself and open it directly.
+If you want to debug the tests in a browser, you will need to somehow host the root directory, for example, using a
+command like `python -m SimpleHTTPServer 7291`.  You would then open `http://localhost:7291/tests/static/all-tests.html`
+in your browser.
--- testem.js
+++ testem.js
@@ -1,24 +1,29 @@
 // Adapted from node ./node_modules/istanbul/lib/cli.js report
 /* eslint-env node */
 "use strict";
-var fluid = require("infusion");
-var gpii  = fluid.registerNamespace("gpii");
+// If we use python to host this directory, this file will be loaded from the browser.  To avoid this, we "gate"
+// The body of the script behind a check to see if `require` exists.
+if (require) {
+    var fluid = require("infusion");
+    var gpii  = fluid.registerNamespace("gpii");
 
-require("./index");
+    require("./index");
 
-gpii.binder.loadTestingSupport();
+    gpii.binder.loadTestingSupport();
 
-var outputFile = fluid.module.resolvePath("%gpii-binder/report.tap");
+    var outputFile = fluid.module.resolvePath("%gpii-binder/report.tap");
 
-var testemHarness = gpii.test.testem({
-    testPages: [ "tests/static/all-tests.html" ],
-    testemOptions: {
-        "framework":   "qunit",
-        "parallel":    1,
-        "report_file": outputFile,
-        "routes": {
-            "/src": "instrumented"
+    var testemHarness = gpii.test.testem({
+        testPages: [ "tests/static/all-tests.html" ],
+        testemOptions: {
+            "framework":   "qunit",
+            "parallel":    1,
+            "report_file": outputFile,
+            "routes": {
+                "/src": "instrumented",
+                "/testem.js": "/"
+            }
         }
-    }
-});
-module.exports = testemHarness.options.testemOptions;
+    });
+    module.exports = testemHarness.options.testemOptions;
+}
