82552d3345303464bbded7f8ea794b26ce986896
Tony Atkins <tony@raisingthefloor.org>
2017-06-14 11:12:29 +0200

    GPII-2296: Updated to released version of gpii-testem. NOJIRA: Fixed linting error.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /* eslint-env node */
 "use strict";
 var fluid = require("infusion");
-var gpii  = fluid.registerNamespace("gpii");
 
 // Register our content so that it can be referenced in other packages using `fluid.module.resolvePath("%gpii-binder/path/to/content")`
 fluid.module.register("gpii-binder", __dirname, require);
